a841a7c7ea5b134e373d6949ed3f534341ccc856
codepractice02 <byra.neeraja@gmail.com>
2022-10-20 11:04:20 -0400

    Create Arthemeticoperations.java
    Sum code

---
+++ Arthemeticoperations.java
@@ -0,0 +1,18 @@
+public class Arthemeticoperations {
+ public static void main (String[]args){
+     int a=20;
+     int b=30;
+     int x=10;
+     double y=10.2;
+     System.out.println("Sum"+(a+b));
+     System.out.println("Sum"+(x+y));
+
+ }
+
+
+
+
+
+
+
+}
